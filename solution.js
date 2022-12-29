3fd030b97ee7404194cce37da29c871ad5f20a3d
RahulAgnihotri1998 <rahulagnihotri1998@gmail.com>
2022-12-29 16:38:41 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,11 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(str) {
+    // Remove all non-alphanumeric characters and convert the string to lowercase
+    str = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
 
+    // Check if the string is a palindrome by comparing it to its reversed version
+    return str === str.split('').reverse().join('');
 }
-module.exports = palindrome
+
+module.exports = palindrome;
